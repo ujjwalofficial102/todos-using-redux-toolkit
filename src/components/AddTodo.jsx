1bc5fd50364b588dd6aaf5dd2e31f09234c87301
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2025-04-01 13:56:14 +0530

    auto focus

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addTodo } from "../features/todo/todoSlice";
 
@@ -6,11 +6,13 @@ function AddTodo() {
   const [input, setInput] = useState("");
   const dispatch = useDispatch();
   const editingText = useSelector((state) => state.editingText);
+  const inputRef = useRef(null);
 
   useEffect(() => {
     if (editingText) {
       setInput(editingText);
     }
+    inputRef.current?.focus(); // Auto-focus on mount
   }, [editingText]);
 
   const addTodoHandler = (e) => {
@@ -18,9 +20,11 @@ function AddTodo() {
     dispatch(addTodo(input));
     setInput("");
   };
+
   return (
     <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
       <input
+        ref={inputRef}
         type="text"
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
         placeholder="Enter a Todo..."
